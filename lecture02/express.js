b8beb59b9f6554fa0c513c79d7077fd30be47599
armanahad6207 <armanahad7549@gmail.com>
2024-04-22 07:02:27 +0530

    middleware and error handling

--- lecture02/express.js
+++ lecture02/express.js
@@ -7,11 +7,31 @@
 
 const express = require("express");
 app = express();
+
+//  middleware => whe use hit any request to server,
+// server accept it and then befor any routing which user
+//  provide there is middleware that we can put.
+// we can put middleware more than one
+
+app.use((req, res, next) => {
+  console.log("its middleware");
+  next();
+});
 app.get("/", function (rep, res) {
   res.send("this is arman");
 });
 app.get("/profile", (req, res) => {
   res.send("my profile");
+});
+
+// erro handling
+
+app.get("/module", (req, res, next) => {
+  return next(new Error("something wrong"));
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(3000);

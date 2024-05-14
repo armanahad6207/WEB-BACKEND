0f61a7ec2179ea645d518db09238a8eaef725a20
armanahad6207 <armanahad7549@gmail.com>
2024-05-14 12:55:36 +0530

    connect to database

--- node/restaurantApi/index.js
+++ node/restaurantApi/index.js
@@ -1,29 +1,29 @@
 const express = require("express"); //third party package
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
 let app = express();
-const users = [
-  {
-    name: "arman",
-    age: 22,
-  },
-  {
-    name: "anil",
-    age: 23,
-  },
-  {
-    name: "ADI",
-    age: 21,
-  },
-  {
-    name: "akash",
-    age: 24,
-  },
-];
+const url = "mongodb://127.0.0.1:27017";
+let db;
 
 app.get("/", (req, res) => {
   res.send(" weilcome to my own server");
 });
 
-app.get("/userList", (req, res) => {
-  res.send(users);
+app.get("/location", async (req, res) => {
+  try {
+    const locations = await db.collection("location").find().toArray();
+    res.send(locations);
+  } catch (err) {
+    console.error("Failed to fetch locations:", err);
+    res.status(500).send("Failed to fetch locations");
+  }
 });
-app.listen(3000);
+
+MongoClient.connect(url, (err, client) => {
+  console.log("connected success fully");
+  if (err) console.log("something wrong");
+  db = client.db("Zomato-Application");
+  app.listen(3000, () => {
+    console.log("server started on the port 3000");
+  });
+});

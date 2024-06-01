e34b076f888f839a844111a1c616ce890500fb8d
armanahad6207 <armanahad7549@gmail.com>
2024-06-01 09:39:05 +0530

    creat login router,i jsonwebtoken

--- node/LoginApp/controller/authController.js
+++ node/LoginApp/controller/authController.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const user = require("../model/userModel");
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
-
+const jwt = require("jsonwebtoken");
+const secert = require("../config");
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+//signup route
 router.post("/register", (req, res) => {
   let hashPassword = bcrypt.hashSync(req.body.password, 8);
   console.log(hashPassword);
@@ -25,6 +27,24 @@ router.post("/register", (req, res) => {
       res.send("registration successfully");
     }
   );
+});
+
+//login route
+
+router.post("/login", (req, res) => {
+  user.findOne({ email: req.body.email }, (err, user) => {
+    if (err) return res.send({ auth: false, token: "error while logging" });
+    if (!user) return res.send({ auth: false, token: "invalid ceredentials" });
+    else {
+      const passIsValid = bcrypt.compareSync(req.body.password, user.password);
+      if (!passIsValid)
+        return res.send({ auth: false, token: "invalid ceredentials" });
+      let token = jwt.sign({ id: user._id }, secert.secert, {
+        expiresIn: 86400,
+      });
+      res.send({ auth: true, token: token });
+    }
+  });
 });
 
 module.exports = router;
